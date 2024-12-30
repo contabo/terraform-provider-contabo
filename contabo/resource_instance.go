95cbad8fd36f09d29452bd77eee731271406fb0a
m0build <173701823+m0build-io@users.noreply.github.com>
2024-12-30 02:16:28 +0100

    use ReinstallInstanceRequest instead of NewReinstallInstanceRequestWithDefaults
    This commit fix the issue where the default_user would be reset to admin
    on reinstallation independently from the value set in terrafom

--- contabo/resource_instance.go
+++ contabo/resource_instance.go
@@ -455,7 +455,7 @@ func updateInstanceValues(d *schema.ResourceData, client *openapi.APIClient, ctx
 }
 
 func reinstall(d *schema.ResourceData, client *openapi.APIClient, ctx context.Context, instanceId int64, diags diag.Diagnostics, m interface{}) diag.Diagnostics {
-	patchInstanceRequest := openapi.NewReinstallInstanceRequestWithDefaults()
+	patchInstanceRequest := &openapi.ReinstallInstanceRequest{}
 
 	if d.HasChange("ssh_keys") {
 		sshKeys := d.Get("ssh_keys")
